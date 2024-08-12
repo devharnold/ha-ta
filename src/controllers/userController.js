67ed48d0e0fc20065a419274a60e9c9ae62f6f0d
Henry Arnold <harnold@localhost.localdomain>
2024-08-12 11:10:27 +0300

    Codee: Controllers, installed jsonwebtoken framework, updated jwt_secret

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,12 @@
-const User = require("../models/models/user");
+const { User } = require("../models/models/user");
 const asyncHandler = require("express-async-handler");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const secretkey = process.env.JWT_SECRET;
 
 // handles user display detail
-exports.user_detial = asyncHandler(async (req, res, next) => {
+/** exports.user_detial = asyncHandler(async (req, res, next) => {
     res.send(`Not Implemented: Author detail: ${req.params.id}`);
 });
 
@@ -10,4 +14,53 @@ exports.user_detial = asyncHandler(async (req, res, next) => {
 exports.user_update_detail = asyncHandler(async (req, res, next) => {
     res.send("Not updated: User profile update");
 });
+*/
+exports.registerUser = async(req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        
+        // hash the password.
+        const hashedPassword = await bcrypt.hash(password, 10);
 
+        //newUser and hash the password.
+        const newUser = await User.create({ name, email, password: hashedPassword });
+        return res.status(201).json(newUser);
+    } catch (error) {
+        return res.status(500).json({ error: 'Error creating user'})
+    }
+}
+
+exports.userLogin = async(req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ where: { username } });
+        if(!user) {
+            return res.status(401).json({ error: 'Invalid credentials' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.passsword);
+        if (!isMatch) {
+            return res.status(401).json({ error: 'Invalid credentials'});
+        }
+        /**const token = jwt.sign({ id: user.id, email: user.email }, 'jwt_token', {
+            expiresIn: '2h',
+        });
+        return res.status(200).json({ token });*/
+        const token = generateToken(user); //generates a token upon user login
+        res.json({ token });
+    } catch (error) {
+        return res.status(500).json({ error: 'Server error', error});
+    }
+}
+
+//function to generate a token
+function generateToken(user) {
+    const payload = {
+        id: user.id,
+        username: user.name,
+        role: user.user
+    };
+
+    return jwt.sign(payload, secretkey, { expiresIn: '2hrs'});
+}
