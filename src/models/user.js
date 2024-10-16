d528c96532c6a30d4c3cc14733901079dd9996ca
Elvis Otieno <125451537+the1Riddle@users.noreply.github.com>
2024-10-16 10:37:26 +0000

    ES6 here we go

--- src/models/user.js
+++ src/models/user.js
@@ -1,18 +1,15 @@
 'use strict';
-// import { userRoute } from '../routes/userRoute';
 import bcrypt from 'bcryptjs';
+import { Model, DataTypes } from 'sequelize';
 
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
+export default (sequelize) => {
   class User extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate = function(models) {
+    static associate(models) {
       // define association here
       User.hasMany(models.Review, {
         foreignKey: 'userId',
@@ -22,8 +19,9 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: 'userID',
         as: 'User'
       });
-    };
+    }
   }
+
   User.init({
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
@@ -32,7 +30,9 @@ module.exports = (sequelize, DataTypes) => {
     password: {
       type: DataTypes.STRING,
       allowNull: false,
-      validate: [8, 16] // sets a minimum of 8 and a maximum of 16 characters within the string.
+      validate: {
+        len: [8, 16] // sets a minimum of 8 and a maximum of 16 characters within the string.
+      }
     },
   }, {
     timestamps: true,
@@ -54,10 +54,20 @@ module.exports = (sequelize, DataTypes) => {
       },
     },
   });
+
   User.prototype.validatePassword = async function(password) {
     return await bcrypt.compare(password, this.password);
   }
+
   return User;
 };
 
-exports.User = User;
+export const User = (sequelize, DataTypes) => {
+  return sequelize.define('User', {
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    phoneNumber: DataTypes.INTEGER,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
+  });
+};
--- src/server.js
+++ src/server.js
@@ -3,40 +3,40 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
+import { userRoute } from './routes/userRoute.js';
+import mysql from 'mysql2';
+
 const app = express();
-import { userRoute } from '../../src/routes/userRoute';
 const hostname = process.env.DB_HOST;
 const PORT = process.env.PORT;
 
-const db = require('./src/models');
-
 app.use(express.json());
-app.use('/api', userRoute)
+app.use('/api', userRoute);
 
 app.get('/', (req, res) => {
     res.status(200).send('Hello, World!');
 }); // test endpoint
 
-app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`);
+app.listen(PORT, hostname, () => {
+    console.log(`Server running at http://${hostname}:${PORT}`);
 }).on('error', (err) => {
     if (err.code == 'EADDRINUSE') {
-        console.error(`Port ${port} is already in use. Please choose another port.`);
+        console.error(`Port ${PORT} is already in use. Please choose another port.`);
         process.exit(1);
     } else {
-        console.error('Server error', err)
+        console.error('Server error', err);
     }
-})
+});
 
-var mysql = require('mysql2');
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
 });
-connection.connect(function(err) {
-    if(err) {
+
+connection.connect((err) => {
+    if (err) {
         console.error('error connecting to mysql server' + err.stack);
         return;
     }
