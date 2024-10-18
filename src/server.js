005e5aad9fb92dd83c2d8452e0778fc895c61f30
Henry Arnold <devharnold@gmail.com>
2024-10-18 14:48:58 +0300

    push

--- src/server.js
+++ src/server.js
@@ -5,36 +5,48 @@ dotenv.config();
 import express from 'express';
 const app = express();
 import { userRoute } from './routes/userRoute.js';
-const hostname = process.env.DB_HOST;
-const port = process.env.PORT;
-import mysql from 'mysql2';
+const HOST = process.env.DB_HOST;
+const PORT = process.env.PORT;
 
 import db from './models/index.js';
 // const db = require('./src/models');
 
-app.use(express.json());
+app.use(express.json()); // Parse json arguments
+
+// define routes 
 app.use('/api', userRoute)
 
 /**app.get('/', (req, res) => {
     res.status(200).send('Hello, World!');
 }); (test endpoint)*/ 
 
-app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`);
+app.listen(PORT, HOST, () => {
+    console.log(`Server running at http://${HOST}:${PORT}`);
 }).on('error', (err) => {
     if (err.code == 'EADDRINUSE') {
-        console.error(`Port ${port} is already in use. Please choose another port.`);
+        console.error(`Port ${PORT} is already in use. Please choose another port.`);
         process.exit(1);
     } else {
         console.error('Server error', err)
     }
 })
 
-const connection = mysql.createConnection({
+import mysql from 'mysql2/promise';
+
+console.log({
     host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT
+})
+
+const connection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME || 'harnold',
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT
 });
 
 connection.connect(function(err) {
